26abb36eabdf6cb1b5ba0d03d928edebe18f3709
Nami <preetheman@gmail.com>
2026-04-29 12:45:53 +0530

    local host port problem in fix mode

--- app.py
+++ app.py
@@ -139,5 +139,7 @@ def debug():
 
 # RUN (only for local)
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    import os
+
+    port = int(os.environ.get("PORT", 5000))  # 👈 key line
+    app.run(debug=True, host="0.0.0.0", port=port)
