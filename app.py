d79303ab1a95312bf565e61ca52c1f10186be5c1
Nami <preetheman@gmail.com>
2026-04-29 13:02:37 +0530

    small changes

--- app.py
+++ app.py
@@ -141,5 +141,5 @@ def debug():
 if __name__ == "__main__":
     import os
 
-    port = int(os.environ.get("PORT", 5000))  # 👈 key line
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
